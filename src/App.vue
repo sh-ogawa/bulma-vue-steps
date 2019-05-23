78074903f44991b88e3637b59fb321c17448a296
sh-ogawa <syogo0417@gmail.com>
2019-05-24 00:45:13 +0900

    add: step-item component.

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+    <section>
+      <steps v-model="newStep">
+        <step-item :label="'test1'">
+          <div class="field is-horizontal">
+            <div class="field-label is-normal">
+              <label class="label">Username</label>
+            </div>
+            <div class="field-body">
+              <div class="field">
+                <div class="control">
+                  <input class="input"
+                         name="username"
+                         id="username"
+                         type="text"
+                         placeholder="Username"
+                         autofocus data-validate="require">
+                </div>
+              </div>
+            </div>
+          </div>
+        </step-item>
+        <step-item :label="'test2'">
+          <div class="field is-horizontal">
+            <div class="field-label is-normal">
+              <label class="label">Password</label>
+            </div>
+            <div class="field-body">
+              <div class="field">
+                <div class="control has-icon has-icon-right">
+                  <input class="input"
+                         type="password"
+                         name="password"
+                         id="password"
+                         placeholder="Password"
+                         data-validate="require">
+                </div>
+              </div>
+            </div>
+          </div>
+        </step-item>
+      </steps>
+    </section>
+</template>
+
+<script>
+import Steps from '@/components/steps/Steps';
+import StepItem from '@/components/steps/StepItem';
+
+export default {
+  name: 'App',
+  components: { StepItem, Steps },
+  data() {
+    return {
+      newStep: 0,
+    };
+  },
+};
+</script>
+
+<style lang="sass">
+  @import "~bulma/bulma.sass"
+  @import "~bulma-steps/dist/css/bulma-steps"
+</style>
---
+++ src/components/steps/StepItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="step-content has-text-centered" :class="'is-active'">
+    hoge
+  </div>
+</template>
+<script>
+export default {
+  name: 'StepItem',
+  props: {
+    label: {
+      type: String,
+      default: '',
+    },
+  },
+  created() {
+    this.$parent.stepItems.push(this);
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
